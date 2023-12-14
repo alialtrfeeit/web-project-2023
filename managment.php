431735ddab2c4c3ce265c51b7193aff45def0cf8
alialtrfeeit <126807463+alialtrfeeit@users.noreply.github.com>
2023-12-14 04:37:37 -0800

    s

--- managment.php
+++ managment.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>managment</title>
-    <link rel="stylesheet" type="text/css" href="tabel.css">
+    <link rel="stylesheet" type="text/css" href="tb.css">
 
 </head>
 <div class="image-container"><img src="imeges/images.png" alt="">
